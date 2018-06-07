36944a46078de6dad5d327cd3788c66ac3bdab9a
Abhi1code <mehariaabhishek@gmail.com>
2018-06-07 18:49:59 +0530

    initial commits by abhi

---
+++ app/src/main/java/com/designrknight/projectconfluence/Landing_page.java
@@ -0,0 +1,21 @@
+package com.designrknight.projectconfluence;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Landing_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this,Sign_in_activity.class);
+        startActivity(intent);
+        finish();
+    }
+}
